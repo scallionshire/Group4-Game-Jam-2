ec3d7bd2e931b9da3e571ba088672198bf990301
ramyzhang <ramyjzh@gmail.com>
2024-01-20 14:37:48 -0500

    hi

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TilemapPuzzle : MonoBehaviour
+public class PuzzleManager : MonoBehaviour
 {
-    public enum TileState
-    {
-        Empty,
-        Block,
-        CorrectTile,
+    public struct PuzzlePiece {
+        public Vector3Int position;
+        public bool isCorrect;
     }
 
     public Tilemap tilemap;
     public TileBase blockTile;
     public TileBase correctPositionTile;
+    public List<bool> correctBlocks;
 
     void Start()
     {
         tilemap = GameObject.Find("Tilemap_Blocks").GetComponent<Tilemap>();
+        correctBlocks = new List<bool>()
+            {
+                false,
+                false,
+                false,
+                false,
+                false,
+            };
     }
 
     void Update()
@@ -28,64 +35,26 @@ public class TilemapPuzzle : MonoBehaviour
         {
             // Puzzle is solved, provide feedback and handle progression
             Debug.Log("Puzzle Solved!");
+            // TODO: Go back to scene and trigger unlock
         }
     }
 
     bool IsPuzzleSolved()
     {
-        BoundsInt bounds = tilemap.cellBounds;
-
         // Iterate through each cell in the tilemap
-        foreach (Vector3Int position in bounds.allPositionsWithin)
+        foreach (bool block in correctBlocks)
         {
-            TileBase currentTile = tilemap.GetTile(position);
-            
-            if (currentTile != null) {
-                Debug.Log(currentTile.name + ": " + position);
-            } else {
-                Debug.Log("null: " + position);
-            }
-            // Check if the tile is a block or correct position
-            if (currentTile == blockTile || currentTile == correctPositionTile)
+            if (!block)
             {
-                // Compare the current tile state with the correct state
-                TileState correctState = GetCorrectState(position);
-
-                if (GetTileState(currentTile) != correctState)
-                {
-                    // Puzzle is not solved, at least one tile is not in the correct state
-                    return false;
-                }
+                // Check for the right tile here
+                Debug.Log("Missing tile: ");
+                return false;
+            } else {
+                Debug.Log("got it!");
             }
         }
 
         // If the loop completes without returning false, the puzzle is solved
         return true;
-    }
-
-    TileState GetTileState(TileBase tile)
-    {
-        if (tile == null)
-        {
-            return TileState.Empty;
-        }
-        else if (tile == blockTile)
-        {
-            return TileState.Block;
-        }
-        else if (tile == correctPositionTile)
-        {
-            return TileState.CorrectTile;
-        }
-
-        return TileState.Empty; // Default to Empty if the tile is not recognized
-    }
-
-    TileState GetCorrectState(Vector3Int position)
-    {
-        // Implement your logic to determine the correct state based on the tile position
-        // This might involve a predefined mapping or a more complex algorithm
-        // Return the correct state for the given position
-        return TileState.CorrectTile; // Placeholder, replace with your logic
     }
 }
