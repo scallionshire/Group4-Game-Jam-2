840e10076677fcec52cd70f729b017c2248558a7
ramyzhang <ramyjzh@gmail.com>
2024-01-20 12:39:56 -0500

    alsdkjf

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -1,19 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class PuzzleManager : MonoBehaviour
+public class TilemapPuzzle : MonoBehaviour
 {
-    [SerializeField] private Tilemap tileMap;
-    // Start is called before the first frame update
-    void Start()
-    {
-        tileMap = GameObject.Find("Tilemap_Blocks").GetComponent<Tilemap>();
-    }
+    private Tilemap tilemap;
+    public TileBase blockTile;
+    public TileBase correctPositionTile;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // void Update()
+    // {
+    //     // Check for puzzle completion
+    //     if (IsPuzzleSolved())
+    //     {
+    //         // Puzzle is solved, provide feedback and handle progression
+    //         Debug.Log("Puzzle Solved!");
+    //     }
+    // }
+
+    // bool IsPuzzleSolved()
+    // {
+    //     BoundsInt bounds = tilemap.cellBounds;
+
+    //     // Iterate through each cell in the tilemap
+    //     foreach (Vector3Int position in bounds.allPositionsWithin)
+    //     {
+    //         TileBase currentTile = tilemap.GetTile(position);
+
+    //         // Check if the tile is a block or correct position
+    //         if (currentTile == blockTile || currentTile == correctPositionTile)
+    //         {
+    //             // Compare the current tile state with the correct state
+    //             TileState correctState = GetCorrectState(position);
+
+    //             if (GetTileState(currentTile) != correctState)
+    //             {
+    //                 // Puzzle is not solved, at least one tile is not in the correct state
+    //                 return false;
+    //             }
+    //         }
+    //     }
+
+    //     // If the loop completes without returning false, the puzzle is solved
+    //     return true;
+    // }
+
+    // TileState GetTileState(TileBase tile)
+    // {
+    //     if (tile == null)
+    //     {
+    //         return TileState.Empty;
+    //     }
+    //     else if (tile == blockTile)
+    //     {
+    //         return TileState.Block;
+    //     }
+    //     else if (tile == correctPositionTile)
+    //     {
+    //         return TileState.CorrectPosition;
+    //     }
+
+    //     return TileState.Empty; // Default to Empty if the tile is not recognized
+    // }
+
+    // TileState GetCorrectState(Vector3Int position)
+    // {
+    //     // Implement your logic to determine the correct state based on the tile position
+    //     // This might involve a predefined mapping or a more complex algorithm
+    //     // Return the correct state for the given position
+    //     return TileState.CorrectPosition; // Placeholder, replace with your logic
+    // }
 }
